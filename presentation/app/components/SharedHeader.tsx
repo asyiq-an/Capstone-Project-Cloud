ba143ea87b8004f66085b26f21fb72fc7d76290c
Asyiq Nuruddin <94519945+AsyiqNuruddin@users.noreply.github.com>
2025-08-12 11:02:25 +0800

    Update SharedHeader.tsx
    v2

--- presentation/app/components/SharedHeader.tsx
+++ presentation/app/components/SharedHeader.tsx
@@ -77,7 +77,7 @@ export default function SharedHeader() {
             height={40}
             className="rounded"
           />
-          <span className="text-xl font-bold text-pink-600">NP Snatch v1</span>
+          <span className="text-xl font-bold text-pink-600">NP Snatch v2</span>
         </div>
       </Link>
 
